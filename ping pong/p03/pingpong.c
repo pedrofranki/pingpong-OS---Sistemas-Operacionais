eb9288712631ce726a72baf6bb31975ec020e458
pedrofranki <pedrofranki2@gmail.com>
2019-04-20 12:54:34 -0300

    termino pingpong.c

--- ping pong/p03/pingpong.c
+++ ping pong/p03/pingpong.c
@@ -1,4 +1,5 @@
 #include "datatypes.h"
+#include "queue.h"
 #include "pingpong.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,8 +7,14 @@
 #define STACKSIZE 32768		/* tamanho de pilha das threads */
 #define _XOPEN_SOURCE 600	/* para compilar no MacOS */
 
-long int id=0;
-task_t mainTask, *exec, *ant;
+void dispatcher_body ();
+task_t *scheduler();
+
+long int id=0, userTasks=0;
+task_t mainTask, *execTask, *ant, *taskQueue;
+task_t dispatcher;
+
+
 
 
 void pingpong_init (){
@@ -15,19 +22,19 @@ void pingpong_init (){
 
     mainTask.next = NULL;
     mainTask.prev = NULL;
-    mainTask.tid = id;
+    mainTask.tid = id++;
 
-    exec = &mainTask;
-    id++;
+    execTask = &mainTask;
+    task_create(&dispatcher, dispatcher_body, NULL);
+
 }
 
 
 int task_create (task_t *task,	void (*start_func)(void *),	 void *arg){
     char* stack;
-
+    task->tid = id++;
     task->next = NULL;
     task->prev = NULL;
-
     getcontext(&(task->context));
 
     stack = malloc(STACKSIZE);
@@ -44,6 +51,18 @@ int task_create (task_t *task,	void (*start_func)(void *),	 void *arg){
       exit (1);
    }
     makecontext(&(task->context), (void *)(*start_func), 1, arg);
+
+    task->tid = id;
+    id++;
+
+    if(task->tid >1){
+      //printf("aaa\n");
+      queue_append((queue_t**)&taskQueue, (queue_t*)task);
+      userTasks++;
+      task->queue = &taskQueue;
+      task->state = 'e';
+    }
+
     task->tid = id;
     id++;
 #ifdef DEBUG
@@ -51,8 +70,6 @@ int task_create (task_t *task,	void (*start_func)(void *),	 void *arg){
 #endif
 
     return task->tid;
-
-
 }
 
 
@@ -60,23 +77,75 @@ void task_exit (int exitCode){
     #ifdef DEBUG
         printf("task_exit: encerrando task %d.\n", taskExec->tid);
     #endif
-    task_switch(&mainTask);
+
+    if(execTask == &dispatcher)
+      task_switch(&mainTask);
+    else
+      task_switch(&dispatcher);
 }
 
 
 int task_switch (task_t *task){
-    ant = exec;
-    exec = task;
+    ant = execTask;
+    execTask = task;
 
     if(swapcontext(&(ant->context), &(task->context))<0){
-        exec = ant;
+        execTask = ant;
         return -1;
     }
-
     return 0;
 }
 
 
 int task_id (){
-    return exec->tid;
+    return execTask->tid;
+}
+
+void task_suspend (task_t *task, task_t **queue) {
+
+  queue_remove((queue_t**)task->queue, (queue_t*)task);
+
+  queue_append((queue_t**)queue, (queue_t*)task);
+  task->state = 's';
+  userTasks--;
+  task->queue = queue;
+}
+
+void task_resume (task_t *task) {
+  queue_remove((queue_t**)task->queue, (queue_t*)task);
+  queue_append((queue_t**)&execTask, (queue_t*)task);
+  task->queue = &execTask;
+  task->state = 'p';
+}
+
+void task_yield () {
+  if(execTask->tid != 0){
+    //printf("b\n");
+    queue_append((queue_t**)&taskQueue, (queue_t*)execTask);
+    execTask->queue = &taskQueue;
+    execTask->state = 'e';
+    userTasks++;
+  }
+  task_switch(&dispatcher);
+}
+
+task_t *scheduler(){
+  userTasks--;
+  return (task_t*) queue_remove((queue_t**)&taskQueue, (queue_t*)taskQueue);
+}
+
+void dispatcher_body () // dispatcher é uma tarefa
+{
+   task_t *next;
+
+   while ( userTasks > 0 ){
+      //printf("%ld\n", userTasks);
+      next = scheduler();
+      next->queue = NULL;
+      if (next){
+         task_switch (next) ; // transfere controle para a tarefa "next"
+      }
+   }
+   printf("carlos\n");
+   task_exit(0) ; // encerra a tarefa dispatcher
 }
---
+++ ping pong/p03/queue.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "queue.h"
+
+
+//------------------------------------------------------------------------------
+// Insere um elemento no final da fila.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - o elemento deve existir
+// - o elemento nao deve estar em outra fila
+
+void queue_append (queue_t **queue, queue_t *elem) {
+    queue_t* aux;
+
+    if(queue == NULL){
+        printf("ERRO: A fila nao existe\n");
+        return;
+    }
+
+    if(elem == NULL){
+        printf("ERRO: O ele");
+        return;
+    }
+
+    if(elem->next!=NULL && elem->prev!=NULL){
+        printf("ERRO: o elemento pertence a outra fila\n");
+        return;
+    }
+
+
+    if((*queue) == NULL){
+        (*queue) = elem;
+        elem->next = elem;
+        elem->prev = elem;
+        return;
+    }
+
+    aux = *queue;
+    if(aux->next == aux && aux->prev == aux){
+        aux->next = elem;
+        aux->prev = elem;
+        elem->next = aux;
+        elem->prev = aux;
+        return;
+    }
+
+
+    //int i=0;
+    while(aux->next != (*queue)){
+        aux = aux->next;
+        //printf("%d \n", i++);
+    }
+    aux->next = elem;
+    elem->prev = aux;
+    elem->next = (*queue);
+    (*queue)->prev = elem;
+
+    //printf("a %d -b %d \n", queue, elem);
+}
+
+//------------------------------------------------------------------------------
+// Remove o elemento indicado da fila, sem o destruir.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - a fila nao deve estar vazia
+// - o elemento deve existir
+// - o elemento deve pertencer a fila indicada
+// Retorno: apontador para o elemento removido, ou NULL se erro
+
+queue_t *queue_remove (queue_t **queue, queue_t *elem) {
+    queue_t* aux;
+    if(queue == NULL) {
+        printf("ERRO: A fila nao existe\n");
+        return NULL;
+    }
+    if((*queue) == NULL){
+        printf("ERRO: A fila esta vazia\n");
+        return NULL;
+    }
+    if(elem == NULL){
+        printf("O elemento nao existe\n");
+        return NULL;
+    }
+
+    aux = (*queue);
+    if(aux->next == aux && aux->prev == aux){//verifica se a fila tem um elemento
+        if((*queue) == elem) {
+            //printf("a\n");
+            *queue == NULL;
+            aux->prev = NULL;
+            aux->next = NULL;
+            return elem;
+        }else
+            return NULL;
+    }else{
+        int i=0;
+        while(aux->next != (*queue)){
+            //printf("\t %d\n", i++);
+
+            if(aux == elem){
+
+                queue_t* ant = aux->prev;
+                queue_t* prox = aux->next;
+
+                ant->next = prox;
+                prox->prev = ant;
+                *queue = prox;
+
+                aux->next = NULL;
+                aux->prev = NULL;
+
+                return aux;
+            }
+            aux=aux->next;
+        }
+        return NULL;
+    }
+
+}
+
+//------------------------------------------------------------------------------
+// Conta o numero de elementos na fila
+// Retorno: numero de elementos na fila
+
+int queue_size (queue_t *queue){
+    queue_t* aux;
+    if(queue == NULL)
+        return 0;
+
+    aux = queue;
+    if(aux->next == aux && aux->prev == aux)
+        return 1;
+    int i=1;
+    while(aux->next != queue){
+        aux = aux->next;
+        i++;
+    }
+    return i;
+}
+
+//------------------------------------------------------------------------------
+// Percorre a fila e imprime na tela seu conteúdo. A impressão de cada
+// elemento é feita por uma função externa, definida pelo programa que
+// usa a biblioteca. Essa função deve ter o seguinte protótipo:
+//
+// void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
+  if(queue != NULL){ //fila existe
+	printf("[");
+        queue_t *elemtmp = queue;
+        while(elemtmp->next != queue){
+            print_elem(elemtmp);
+            elemtmp = elemtmp->next;
+	    if(elemtmp->next != queue){printf(" ");}
+        }
+	printf("]\n");
+    }else{
+	printf("[]");}
+
+
+}
